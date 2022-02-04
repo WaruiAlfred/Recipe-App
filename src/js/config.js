59c39983c170d0a4bb1b3a50ca9ae333e726f2d7
WaruiAlfred <alfredwarui17@gmail.com>
2022-02-04 12:10:35 +0300

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '2e0fc7ee-8dcc-494a-9448-da1c87285ae3';
+export const MODAL_CLOSE_SEC = 2.5;
